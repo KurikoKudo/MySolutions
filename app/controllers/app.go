5208da9e08bbe3fa23ac9b37e96d6a50714c007e
Kuriko <e155407w@st.u-gakugei.ac.jp>
2018-01-28 18:19:43 +0900

    finish form

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"github.com/revel/revel"
+	"strings"
+	"fmt"
 )
 
 type App struct {
@@ -16,4 +18,33 @@ func (c App) Index() revel.Result {
 func (c App) Home() revel.Result {
 	text := "- welcome to YourSolutions -"
 	return c.Render(text)
+}
+
+
+
+/*POST*/
+func (c App) Search() revel.Result {
+
+var all []string
+var ptitle []string
+var tags []string
+var ecode string
+
+
+if(c.Params.Form.Get("ecode") != ""){
+	ecode = c.Params.Form.Get("ecode")
+}
+
+
+	all = strings.Split((c.Params.Form.Get("all")), " ")
+
+	ptitle = strings.Split((c.Params.Form.Get("ptitle")), " ")
+
+	tags = strings.Split((c.Params.Form.Get("tags")), " ")
+	fmt.Println(len(all))
+
+
+
+
+	return c.Render(all,ptitle,tags,ecode)
 }
